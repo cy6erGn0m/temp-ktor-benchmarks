f758b97984a0ff33f187bd79edd2c713b4f01bc6
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2016-09-06 20:03:14 +0300

    Add concurrency 100 default

--- core/src/main/kotlin/org/jetbrains/ktor/benchmarks/Benchmark.kt
+++ core/src/main/kotlin/org/jetbrains/ktor/benchmarks/Benchmark.kt
@@ -2,4 +2,4 @@ package org.jetbrains.ktor.benchmarks
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FUNCTION)
-annotation class Benchmark(val concurrency: IntArray = intArrayOf(1, 4, 10, 50), val iterationsLimit: Int = 1000, val maxDurationMillis: Long = 10000, val minDurationMillis: Long = 1000)
+annotation class Benchmark(val concurrency: IntArray = intArrayOf(1, 4, 10, 50, 100), val iterationsLimit: Int = 1000, val maxDurationMillis: Long = 10000, val minDurationMillis: Long = 1000)
