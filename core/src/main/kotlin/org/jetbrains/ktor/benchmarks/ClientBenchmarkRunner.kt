e3affab344b9af2b730bb59b3826ea2b502372bc
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2016-09-06 12:54:41 +0300

    render charts

--- core/src/main/kotlin/org/jetbrains/ktor/benchmarks/ClientBenchmarkRunner.kt
+++ core/src/main/kotlin/org/jetbrains/ktor/benchmarks/ClientBenchmarkRunner.kt
@@ -6,8 +6,14 @@ import org.junit.runner.*
 import org.junit.runner.notification.*
 import org.junit.runners.*
 import org.junit.runners.model.*
+import org.knowm.xchart.*
+import org.knowm.xchart.style.*
+import java.awt.*
+import java.awt.image.*
+import java.io.*
 import java.math.*
 import java.time.*
+import javax.imageio.*
 import kotlin.reflect.*
 
 class ClientBenchmarkRunner(clazz: Class<*>) : ParentRunner<FrameworkMethod>(clazz) {
@@ -52,7 +58,8 @@ class ClientBenchmarkRunner(clazz: Class<*>) : ParentRunner<FrameworkMethod>(cla
         val concurrency = benchmarkAnnotation.concurrency
         val maxCount = benchmarkAnnotation.iterationsLimit
         val minCount = Math.max(100, maxCount)
-        val maxDuration = benchmarkAnnotation.maxDurationMillis
+        val maxDurationMillis = benchmarkAnnotation.maxDurationMillis
+        val minDurationMills = benchmarkAnnotation.minDurationMillis
 
         testClass.annotatedMethods.filter { it.getAnnotation(Before::class.java) != null }.forEach { m ->
             m.invokeExplosively(instance)
@@ -73,7 +80,7 @@ class ClientBenchmarkRunner(clazz: Class<*>) : ParentRunner<FrameworkMethod>(cla
                     var count = 0
                     val start = System.currentTimeMillis()
 
-                    while (count < maxCount && (count < minCount || ((System.currentTimeMillis() - start) < maxDuration))) {
+                    while (count < maxCount && (System.currentTimeMillis() - start) < maxDurationMillis && (count < minCount || ((System.currentTimeMillis() - start) < minDurationMills))) {
                         count++
                         child.invokeExplosively(instance)
                     }
@@ -96,7 +103,40 @@ class ClientBenchmarkRunner(clazz: Class<*>) : ParentRunner<FrameworkMethod>(cla
                     metrics.keys.sorted().forEach { name ->
                         val m = metrics[name]!!
 
-                        println("    ${m.label.padEnd(maxLabelLength)} ${m.avg.prettyPrint()} (${m.min.prettyPrint()} .. ${m.max.prettyPrint()})")
+                        println("    ${m.label.padEnd(maxLabelLength)} (${m.points} pts) ${m.avg.prettyPrint()} (${m.min.prettyPrint()} .. ${m.max.prettyPrint()})")
+                    }
+
+                    // render chart
+
+                    for ((m, data) in groupedBy) {
+                        val chart = XYChartBuilder()
+                                .width(800).height(600)
+                                .title("${child.name} $m")
+                                .xAxisTitle("Concurrency").yAxisTitle("Time")
+                                .build()
+
+                        chart.styler.theme = GGPlot2Theme()
+                        chart.styler.defaultSeriesRenderStyle = XYSeries.XYSeriesRenderStyle.Scatter
+                        chart.styler.markerSize = 8
+                        chart.styler.isYAxisLogarithmic = true
+//                        chart.styler.isLegendVisible = false
+
+                        val yPoints = data.mapNotNull { it.laps.lastOrNull()?.fromStart?.toMillisExact()?.toDouble() }.toDoubleArray()
+                        chart.addSeries(testClass.name.substringAfterLast('.'), yPoints.map { 1.0 }.toDoubleArray(), yPoints)
+
+                        val image =
+                                if (GraphicsEnvironment.isHeadless()) BufferedImage(800, 600, BufferedImage.TYPE_4BYTE_ABGR)
+                                else GraphicsEnvironment.getLocalGraphicsEnvironment().defaultScreenDevice.defaultConfiguration.createCompatibleImage(800, 600, java.awt.Transparency.TRANSLUCENT)
+
+                        image.createGraphics().apply {
+                            chart.paint(this, 800, 600)
+                            dispose()
+                        }
+
+                        val file = File("target/charts/${safePathComponent(child.name)}/${safePathComponent(m)}.png")
+                        file.parentFile.mkdirs()
+
+                        ImageIO.write(image, "PNG", file)
                     }
                 }
             }
@@ -111,7 +151,7 @@ class ClientBenchmarkRunner(clazz: Class<*>) : ParentRunner<FrameworkMethod>(cla
         return Description.createTestDescription(testClass.name, child.name)
     }
 
-    data class Metric(val label: String, val min: Duration, val max: Duration, val avg: Duration)
+    data class Metric(val label: String, val min: Duration, val max: Duration, val avg: Duration, val points: Int)
 
     private fun findRules(instance: Any) = testClass.getAnnotatedFieldValues(instance, Rule::class.java, TestRule::class.java) +
             testClass.getAnnotatedMethodValues(instance, Rule::class.java, TestRule::class.java)
@@ -127,15 +167,28 @@ class ClientBenchmarkRunner(clazz: Class<*>) : ParentRunner<FrameworkMethod>(cla
         val max = times.max()!!
         val avg = times.map { BigInteger.valueOf(it)!! }.reduce(BigInteger::plus).div(BigInteger.valueOf(size.toLong())).longValueExact()
 
-        return Metric(first().label, Duration.ofNanos(min), Duration.ofNanos(max), Duration.ofNanos(avg))
+        return Metric(first().label, Duration.ofNanos(min), Duration.ofNanos(max), Duration.ofNanos(avg), times.size)
     }
 
     private fun Duration.prettyPrint(): String {
         val millis = nano.toLong() / 1000000L
         val nanos = nano.toLong() % 1000000L
 
-        return listOf(seconds to "s", millis to "ms", nanos to "ns").filter { it.first != 0L }.joinToString(" ") { "${it.first} ${it.second}" }
+        val result = mutableListOf(seconds.toString() to "s")
+        if (nanos < 1000) {
+            result += millis.toString() to "ms"
+            result += nanos.toString() to "ns"
+        } else {
+            val ms = BigDecimal.valueOf(millis) + BigDecimal.valueOf(nanos).divide(BigDecimal.valueOf(1000000L))
+            result += ms.stripTrailingZeros().toPlainString() to "ms"
+        }
+
+        return result.filter { it.first != "0" }.joinToString(" ") { "${it.first} ${it.second}" }
     }
+
+    private fun safePathComponent(c: String) = c.replace("[^\\w\\d]+".toRegex(), " ").trim().replace("\\s+".toRegex(), " ")
+
+    private fun Duration.toMillisExact() = BigDecimal.valueOf(toMillis()) + BigDecimal.valueOf(nano.toLong()).divide(BigDecimal.valueOf(1000000L))
 
     private fun warmUpTimer(timer: Timer) {
         timer.start("").apply {
